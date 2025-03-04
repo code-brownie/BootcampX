04e9c107f07520c8ab25194da8533811100e8661
Aman <jhonman02@gmail.com>
2025-03-04 14:51:30 +0530

    hello.cpp file changed

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the Value of n: ";
+    cin >> n;
+    cout << "The value of n is : " << n;
+    return 0;
+}
